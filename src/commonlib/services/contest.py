4fd5e463da3a28172d747296447e0f1c03f7fe82
nadare <hor7or@ya.ru>
2025-01-25 12:53:11 +0300

    add convenient contest client

---
+++ src/commonlib/services/contest.py
@@ -0,0 +1,65 @@
+import httpx
+
+from ..models import ContestAction, Wizard
+
+
+class ContestClient:
+    def __init__(self, base_url: str):
+        self.client = httpx.AsyncClient(base_url=base_url)
+
+    async def close(self):
+        await self.client.aclose()
+
+    async def create_director(self) -> int:
+        response = await self.client.post("/create_director")
+        response.raise_for_status()
+        return response.json()
+
+    async def set_wizard(self, director_id: int, user_id: int, wizard: Wizard) -> None:
+        response = await self.client.post(
+            "/set_wizard",
+            json={
+                "director_id": director_id,
+                "user_id": user_id,
+                "wizard": wizard.model_dump()
+            }
+        )
+        response.raise_for_status()
+
+    async def get_user_to_make_turn(self, director_id: int) -> int:
+        response = await self.client.post(
+            "/get_user_to_make_turn",
+            json={"director_id": director_id}
+        )
+        response.raise_for_status()
+        return response.json()
+
+    async def get_available_spells(self, director_id: int, user_id: int) -> list[int]:
+        response = await self.client.post(
+            "/get_available_spells",
+            json={
+                "director_id": director_id,
+                "user_id": user_id
+            }
+        )
+        response.raise_for_status()
+        return response.json()
+
+    async def cast_spell(self, director_id: int, user_id: int, spell_id: int) -> None:
+        response = await self.client.post(
+            "/cast_spell",
+            json={
+                "director_id": director_id,
+                "user_id": user_id,
+                "spell_id": spell_id
+            }
+        )
+        response.raise_for_status()
+
+    async def get_action(self, director_id: int) -> ContestAction:
+        response = await self.client.post(
+            "/get_action",
+            json={"director_id": director_id}
+        )
+        response.raise_for_status()
+        return ContestAction(**response.json())
